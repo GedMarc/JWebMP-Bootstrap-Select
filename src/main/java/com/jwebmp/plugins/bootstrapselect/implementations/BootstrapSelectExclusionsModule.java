37495e3db532cd62009526f50c3c2af76fa94ea8
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:15 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

---
+++ src/main/java/com/jwebmp/plugins/bootstrapselect/implementations/BootstrapSelectExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.bootstrapselect.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BootstrapSelectExclusionsModule
+		implements IGuiceScanModuleExclusions<BootstrapSelectExclusionsModule>,
+				           IGuiceScanJarExclusions<BootstrapSelectExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-bootstrap-select-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.bootstrapselect");
+		return strings;
+	}
+}
